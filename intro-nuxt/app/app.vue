9dee7e979812a6171558122a2a402a3f1ee306c4
batata <contactobrite@gmail.com>
2026-02-21 15:39:35 +0000

    intro nuxt

--- intro-nuxt/app/app.vue
+++ intro-nuxt/app/app.vue
@@ -1,21 +1,11 @@
-<script lang="ts" setup>
-
-
-const count = ref(5)
-
-await sleep(2000)
-count.value = 20
-</script>
-
 <template>
-  <div>
-    <h1>Hello, World! - {{ count }}</h1>
-    <button @click="count++">Click me+</button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
-<!-- <style>
-body {
+<style>
+/* body {
   background-color: #020420;
   font-family: 'Roboto', sans-serif;
   color: #fff;
@@ -28,5 +18,5 @@ h4,
 h5,
 h6 {
   color: #00dc82;
-}
-</style> -->
+} */
+</style>
