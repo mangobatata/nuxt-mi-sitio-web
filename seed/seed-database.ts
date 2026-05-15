99f78e974f980367f332f7720e3aa76cdae98588
batata <contactobrite@gmail.com>
2026-05-15 19:34:33 +0000

    server endpoint con query param

--- seed/seed-database.ts
+++ seed/seed-database.ts
@@ -1,19 +1,40 @@
 import "dotenv/config";
+import { randomUUID } from "node:crypto";
+
 import prisma from "../lib/prisma.ts";
+import { products } from "./products.seed.ts";
 import { siteReviews } from "./site-review.seed.ts";
+
+function generateSlug(name: string) {
+  const id = randomUUID().slice(0, 6);
+
+  const nameSlug = name
+    .toLowerCase()
+    .trim()
+    .replace(/&/g, "and")
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+
+  return `${id}-${nameSlug}`;
+}
 
 async function seedDatabase() {
   // Purgar base de datos
   await prisma.siteReview.deleteMany();
   await prisma.product.deleteMany();
 
-  // Insertar registros
+  // Insertar reviews
   await prisma.siteReview.createMany({
     data: siteReviews,
   });
 
+  // Insertar productos con slug generado automáticamente
   await prisma.product.createMany({
-    data: products,
+    data: products.map((product) => ({
+      ...product,
+      slug: generateSlug(product.name),
+    })),
   });
 
   console.log("Database seeded successfully");
---
+++ server/api/products/index.get.ts
@@ -0,0 +1,28 @@
+import prisma from "@@/lib/prisma";
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+
+  let limit = parseInt(query.limit as string) || 10;
+  let offset = parseInt(query.offset as string) || 0;
+
+  // Validaciones
+  if (isNaN(limit) || limit <= 1) limit = 10;
+  if (isNaN(offset) || offset < 0) offset = 0;
+
+  const products = await prisma.product.findMany({
+    take: limit,
+    skip: offset,
+  });
+
+  const total = await prisma.product.count();
+  const totalPages = Math.ceil(total / limit);
+
+  return {
+    products,
+    totalPages,
+    currentPage: offset / limit + 1,
+    perPage: limit,
+    total,
+  };
+});
