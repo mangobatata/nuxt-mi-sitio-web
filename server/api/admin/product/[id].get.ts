cd1c1c90e9e93ccc282599ead4cfe85787003ef3
batata <contactobrite@gmail.com>
2026-05-16 22:42:12 +0000

    Server API Middlewares

--- server/api/admin/product/[id].get.ts
+++ server/api/admin/product/[id].get.ts
@@ -4,16 +4,17 @@ export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, "id") as string;
 
   // Verificar sesión y roles del usuario
-  const session = await requireUserSession(event);
-  // const session = await getUserSession(event);
-  const hasAdminRole = session.user.roles.includes("admin");
+  // Esto lo vamos a validar en un middleware en server/middleware
+  //   const session = await requireUserSession(event);
+  //   // const session = await getUserSession(event);
+  //   const hasAdminRole = session.user.roles.includes("admin");
 
-  if (!hasAdminRole) {
-    throw createError({
-      statusCode: 401,
-      message: `Unauthorized`,
-    });
-  }
+  //   if (!hasAdminRole) {
+  //     throw createError({
+  //       statusCode: 401,
+  //       message: `Unauthorized`,
+  //     });
+  //   }
 
   const product = await prisma.product.findUnique({
     where: {
